61dd8075e637d67ffe248506a9be9c7d730b4acd
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-08-20 20:54:18 -0500

    Reubicamos los datos, Configuramos rutas con carga perezosa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 
 import { Routes } from '@angular/router';
-import { CourseList } from './features/course/components/course-list/course-list';
+// import { CourseList } from './features/course/components/course-list/course-list';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/courses', pathMatch: 'full'},
     {
-        path: '',
-        component: CourseList
+        path: 'courses',
+        loadChildren: () => import('./features/course/courses.routes').then(m => m.COURSES_ROUTES)
+    },
+    {
+        path: '**', redirectTo: '/courses'
     }
 ];
